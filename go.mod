5b15b910aecb989eac388cff0d0f146b916ea9c1
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-10-18 21:26:52 +0530

    fixed the error during file execution

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/hackeramitkumar/slack-bots
 go 1.20
 
 require (
-	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.1 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/shomali11/slacker v1.4.1
+	github.com/slack-go/slack v0.12.3
+)
+
+require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/shomali11/commander v0.0.0-20220716022157-b5248c76541a // indirect
 	github.com/shomali11/proper v0.0.0-20180607004733-233a9a872c30 // indirect
-	github.com/shomali11/slacker v1.4.1 // indirect
-	github.com/slack-go/slack v0.12.3 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 	"log"
 	"os"
 	"strconv"
-
+    "github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
 	"github.com/slack-go/slack"
 )
+
+func loadEnvVariables() error {
+    err := godotenv.Load(".env")
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
 
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
@@ -58,10 +67,23 @@ func custom_bot() {
 func upload_file() {
 
 	api := slack.New(os.Getenv("SLACK_FILE_BOT_TOKEN"))
+	if api == nil {
+		fmt.Printf("Error creating slack client\n")
+		return
+	}
 	channelArr := []string{os.Getenv("CHANNEL_ID")}
-	fileArr := []string{"files/amit.pdf", "files/capi.yaml"}
+	fileArr := []string{"files/amit.pdf", "files/capi.yaml","files/test1.txt","files/test2.txt"}
 
 	for i := 0; i < len(fileArr); i++ {
+		  fileInfo, err := os.Stat(fileArr[i])
+        if err != nil {
+            fmt.Printf("File not found or error accessing file: %s\n", fileArr[i])
+            continue // Skip this file and continue with the next one
+        }
+		      if fileInfo.IsDir() {
+            fmt.Printf("Skipping directory: %s\n", fileArr[i])
+            continue // Skip directories
+        }
 		params := slack.FileUploadParameters{
 			Channels: channelArr,
 			File:     fileArr[i],
@@ -75,6 +97,7 @@ func upload_file() {
 }
 
 func main() {
-	upload_file()
+	loadEnvVariables()
+    upload_file()
 	custom_bot()
 }
